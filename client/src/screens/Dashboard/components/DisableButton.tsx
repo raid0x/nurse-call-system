f383cd3d87c489f2003ba38872099f795b2f37cb
Raidox <dev@raidox.gg>
2019-01-21 12:30:27 -0800

    auto reconnect websocket

--- client/src/screens/Dashboard/components/DisableButton.tsx
+++ client/src/screens/Dashboard/components/DisableButton.tsx
@@ -3,21 +3,34 @@ import * as React from "react";
 import { env } from "../../../config";
 
 class DisableButton extends React.Component<{}, { socket: WebSocket }> {
-  constructor(props) {
-    super(props);
-
-    this.state = { socket: new WebSocket(env.webSocketUrl) };
-  }
+  state = { socket: new WebSocket(env.webSocketUrl) };
 
   componentWillUnmount() {
     this.state.socket.close();
   }
 
-  render() {
+  handleClick = () => {
     const { socket } = this.state;
 
+    if (socket.readyState != socket.OPEN) {
+      this.setState({ socket: new WebSocket(env.webSocketUrl) }, () => {
+        const interval = setInterval(() => {
+          const { socket } = this.state;
+
+          if (socket.readyState == socket.OPEN) {
+            this.state.socket.send("deactivate");
+            clearInterval(interval);
+          }
+        }, 50);
+      });
+    } else {
+      socket.send("deactivate");
+    }
+  };
+
+  render() {
     return (
-      <button type="button" onClick={() => socket.send("deactivate")}>
+      <button type="button" onClick={this.handleClick}>
         DISABLE
       </button>
     );
