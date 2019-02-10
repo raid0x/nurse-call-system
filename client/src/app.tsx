a04f4a2631a779f3bf5cad7183042d9e26fa15d0
Raidox <dev@raidox.gg>
2019-02-09 17:57:49 -0800

    update code style

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-import Dashboard from "./screens/Dashboard";
+import Dashboard from './screens/Dashboard';
 
 const App = () => <Dashboard />;
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
